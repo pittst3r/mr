48422b9e958e24f4c888e0b293efcc3695546684
Robbie Pitts <me@robbie-pitts.com>
2020-07-17 01:18:19 -0400

    More refactoring

--- src/main.rs
+++ src/main.rs
@@ -24,71 +24,91 @@ struct Cli {
 
 struct Mr {
     root: path::PathBuf,
-    pattern: path::PathBuf,
 }
 
 impl Mr {
-    fn new(pattern: path::PathBuf) -> Mr {
+    fn new() -> Mr {
         let start = env::current_dir().unwrap_or(path::PathBuf::from("."));
         let root = find_root_dir(&start).unwrap_or(path::PathBuf::from("."));
 
-        Mr { root, pattern }
+        Mr { root }
     }
 
-    fn cwd(self) -> io::Result<path::PathBuf> {
-        if self.pattern == path::PathBuf::from("-") {
-            return Ok(self.pattern);
+    fn cwd(self, pattern: path::PathBuf) -> io::Result<path::PathBuf> {
+        if pattern == path::PathBuf::from("-") {
+            return Ok(pattern);
         }
 
         let start = env::current_dir()?;
 
-        if self.pattern == path::PathBuf::from("/") {
+        if pattern == path::PathBuf::from("/") {
             return Ok(self.root);
         }
 
-        package_path(self.root, start, &self.pattern)?.canonicalize()
-    }
-}
-
-fn main() -> Result<(), io::Error> {
-    let args = Cli::from_args();
-    let list = args.list;
-
-    if list {
-        print!("echo \"{}\"", list_package_directories()?);
-        return Ok(());
+        self.package_path(start, &pattern)?.canonicalize()
     }
 
-    let mr = Mr::new(args.dir.unwrap_or(path::PathBuf::from(".")));
-    let cwd = mr.cwd()?;
+    fn list_package_directories(self) -> io::Result<String> {
+        let mut result = String::new();
+        let packages = match self.find_package_directories() {
+            Ok(pkgs) => pkgs,
+            Err(_e) => Vec::new(),
+        };
 
-    match args.script {
-        Some(s) => print!("yarn run --cwd={} {}", cwd.display(), s),
-        None => print!("cd {}", cwd.display()),
+        for pkg in packages {
+            result.push_str(pkg.to_str().unwrap_or(""));
+            result.push_str("\n");
+        }
+
+        Ok(result)
     }
 
-    Ok(())
-}
+    fn package_path(
+        self,
+        base: path::PathBuf,
+        pattern: &path::PathBuf,
+    ) -> io::Result<path::PathBuf> {
+        let full_path = base.join(pattern);
 
-fn package_path(
-    root: path::PathBuf,
-    base: path::PathBuf,
-    pattern: &path::PathBuf,
-) -> io::Result<path::PathBuf> {
-    let full_path = base.join(pattern);
+        if full_path.exists() {
+            return Ok(full_path);
+        }
 
-    if full_path.exists() {
-        return Ok(full_path);
+        if self.root == base {
+            return Err(io::Error::new(
+                io::ErrorKind::NotFound,
+                "Could not find given directory within project",
+            ));
+        }
+
+        self.package_path(base.join(path::PathBuf::from("..")), pattern)
     }
 
-    if root == base {
-        return Err(io::Error::new(
-            io::ErrorKind::NotFound,
-            "Could not find given directory within project",
-        ));
-    }
+    fn find_package_directories(self) -> Result<Vec<path::PathBuf>, Box<dyn std::error::Error>> {
+        let package_json = String::from_utf8(fs::read(self.root.join("package.json"))?)?;
+        let patterns = &json::parse(&package_json)?["workspaces"]["packages"];
+        let mut workspaces = Vec::new();
 
-    package_path(root, base.join(path::PathBuf::from("..")), pattern)
+        for pattern in patterns.members() {
+            let full_pattern = match pattern.as_str() {
+                Some(p) => self.root.join(p),
+                None => self.root.to_path_buf(),
+            };
+            let pattern_str = match full_pattern.to_str() {
+                Some(p) => p,
+                None => return Err(Box::new(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "There may be an invalid unicode character in your workspace package patterns",
+                ))),
+            };
+
+            for dir in glob(pattern_str)? {
+                workspaces.push(dir?);
+            }
+        }
+
+        Ok(workspaces)
+    }
 }
 
 fn find_root_dir(current: &path::PathBuf) -> io::Result<path::PathBuf> {
@@ -113,47 +133,23 @@ fn find_root_dir(current: &path::PathBuf) -> io::Result<path::PathBuf> {
     find_root_dir(&next)
 }
 
-fn find_package_directories(
-    root: &path::PathBuf,
-) -> Result<Vec<path::PathBuf>, Box<dyn std::error::Error>> {
-    let package_json = String::from_utf8(fs::read(root.join("package.json"))?)?;
-    let patterns = &json::parse(&package_json)?["workspaces"]["packages"];
-    let mut workspaces = Vec::new();
+fn main() -> Result<(), io::Error> {
+    let args = Cli::from_args();
+    let list = args.list;
+    let mr = Mr::new();
 
-    for pattern in patterns.members() {
-        let full_pattern = match pattern.as_str() {
-            Some(p) => root.join(p),
-            None => root.to_path_buf(),
-        };
-        let pattern_str =
-            match full_pattern.to_str() {
-                Some(p) => p,
-                None => return Err(Box::new(io::Error::new(
-                    io::ErrorKind::InvalidData,
-                    "There may be an invalid unicode character in your workspace package patterns",
-                ))),
-            };
-
-        for dir in glob(pattern_str)? {
-            workspaces.push(dir?);
-        }
+    if list {
+        print!("echo \"{}\"", mr.list_package_directories()?);
+        return Ok(());
     }
 
-    Ok(workspaces)
-}
+    let pattern = args.dir.unwrap_or(path::PathBuf::from("."));
+    let cwd = mr.cwd(pattern)?;
 
-fn list_package_directories() -> io::Result<String> {
-    let mut result = String::new();
-    let root = find_root_dir(&env::current_dir()?)?;
-    let packages = match find_package_directories(&root) {
-        Ok(pkgs) => pkgs,
-        Err(_e) => Vec::new(),
-    };
-
-    for pkg in packages {
-        result.push_str(pkg.to_str().unwrap_or(""));
-        result.push_str("\n");
+    match args.script {
+        Some(s) => print!("yarn run --cwd={} {}", cwd.display(), s),
+        None => print!("cd {}", cwd.display()),
     }
 
-    Ok(result)
+    Ok(())
 }
